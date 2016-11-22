c15fc94248aa525cb0f017e9f6176b0af73567f7
Benjamin Youngs <youngs.ben@gmail.com>
2016-11-21 23:17:18 -0500

    Updated eBay-python.py

--- ebay-python.py
+++ ebay-python.py
@@ -1,1 +1,22 @@
-print "Test"
+import datetime, sys, os
+from ebaysdk.exception import ConnectionError
+from ebaysdk.finding import Connection
+
+ebayappid = os.environ['APPID']
+print ebayappid
+
+try:
+    api = Connection(appid=ebayappid, config_file=None)
+    response = api.execute('findItemsAdvanced', {'keywords': 'legos'})
+
+    assert(response.reply.ack == 'Success')
+    assert(type(response.reply.timestamp) == datetime.datetime)
+    assert(type(response.reply.searchResult.item) == list)
+
+    item = response.reply.searchResult.item[0]
+    assert(type(item.listingInfo.endTime) == datetime.datetime)
+    assert(type(response.dict()) == dict)
+
+except ConnectionError as e:
+    print(e)
+    print(e.response.dict())
